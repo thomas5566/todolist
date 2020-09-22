0a745bc1749fb02836876016a496f4049f68fd6e
thomas5566 <thomas19840826@gmail.com>
2020-09-23 00:00:49 +0800

    All Goodgit add -A

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required  # 只有登入者才能造訪網頁
+
 from .forms import TodoForm
 from .models import Todo
 
@@ -70,12 +73,14 @@ def loginuser(request):
             return redirect("currenttodos")
 
 
+@login_required
 def logoutuser(request):
     if request.method == "POST":  # logout must be POST
         logout(request)
         return redirect("home")
 
 
+@login_required
 def createtodo(request):
     if request.method == "GET":
         return render(request, "todo/createtodo.html", {"form": TodoForm()})
@@ -97,6 +102,7 @@ def createtodo(request):
             )
 
 
+@login_required
 def currenttodos(request):
     todos = Todo.objects.filter(
         user=request.user, datecompleted__isnull=True  # 如果datecompleted is null 就不show
@@ -104,13 +110,24 @@ def currenttodos(request):
     return render(request, "todo/currenttodos.html", {"todos": todos})
 
 
+@login_required
+def completedtodos(request):
+    todos = Todo.objects.filter(
+        user=request.user, datecompleted__isnull=False  # 事件完成設為False
+    ).order_by(
+        "-datecompleted"
+    )  # filter 可以設定model要找的參數, model user = request user
+    return render(request, "todo/completedtodos.html", {"todos": todos})
+
+
+@login_required
 def viewtodo(request, todo_pk):  # 丟url.py設定的todo_pk到這
     # 從modle撈pk key資料需使用get_object_or_404
     # user=request.user 確保user只能更改自己的TODO 不然從URL更改ID就可以看到別人的資訊 並做update
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     # grap data form TodoForm
     if request.method == "GET":
-        form = TodoForm(instance=todo)
+        form = TodoForm(instance=todo)  # grap data form TodoForm
         return render(request, "todo/viewtodo.html", {"todo": todo, "form": form})
     else:
         try:
@@ -125,4 +142,21 @@ def viewtodo(request, todo_pk):  # 丟url.py設定的todo_pk到這
                 "todo/viewtodo.html",
                 {"todo": todo, "form": form, "error": "Bad info"},
             )
+
+
+@login_required
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == "POST":
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect("currenttodos")
+
+
+@login_required
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == "POST":
+        todo.delete()
+        return redirect("currenttodos")
 
--- todowooo/settings.py
+++ todowooo/settings.py
@@ -113,3 +113,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+LOGIN_URL = "/login"  # path的login 名稱, 重新導向至login page
